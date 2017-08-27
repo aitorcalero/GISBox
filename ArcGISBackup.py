8aee5d2879b534818b9328f8f57131d5a9013ff3
Aitor Calero <aitor.calero@esri.es>
2017-08-27 13:08:04 +0200

    Full folder structure copy & downloading of files (CSV, Service Definition only)

--- ArcGISBackup.py
+++ ArcGISBackup.py
@@ -2,25 +2,33 @@ from arcgis.gis import GIS
 from arcgis.gis import User
 import os
 import getpass
+import shutil
+from pathlib import Path
 
 url = 'https://geogeeks.maps.arcgis.com'
 usr = 'aitor.calero.masquemapas'
 directorio = os.getcwd()
 #p = getpass.getpass()
 p = 'EkE4HEhpmRE8'
+arcgisboxdir = "/Users/aitorcalero/PycharmProjects/ArcGISBox/Box/"
 
 
-def connnect_to_arcgis(_url, _usr, _pwd):
+def connect_to_arcgis(_url, _usr, _pwd):
     gis = GIS(_url, _usr, _pwd)
     print('Connected successfully to the [' + gis.properties.name + '] organization\n')
     return gis
 
 
-gis = connnect_to_arcgis(url, usr, p)
+gis = connect_to_arcgis(url, usr, p)
 
 m = gis.map()
 user = User(gis, usr, userdict=None)
+
+if Path(arcgisboxdir).exists():
+    shutil.rmtree(arcgisboxdir)
+
 os.mkdir('Box')
+
 
 
 def CreateFolderStructure():
@@ -33,30 +41,31 @@ def CreateFolderStructure():
     print("Total number of items in root folder: " + str(len(root_folder_items)))
 
     # list of supported file types to retrieve from the user folders
-    file_types = ['CSV', 'Microsoft Excel', 'KML', 'PDF', 'ZIP', 'Service Definition']
+    #file_types = ['CSV', 'Microsoft Excel', 'KML', 'PDF', 'ZIP', 'Service Definition']
+    file_types = ['CSV','Service Definition']
 
-    # Listing items in the root folder
+    # Listing & downloading items in the root folder
     for root_folder_item in root_folder_items:
         if (root_folder_item.type in file_types):
             n += 1
+            item_path = str(root_folder_item.get_data())
+            os.rename(str(item_path),arcgisboxdir+root_folder_item.title)
             print(root_folder_item.title + '\t\t(' + root_folder_item.type + ')\n')
 
     # Listing documents inside other user folders
     for fld in flds:
         # print((carpeta))
         print('Name: ' + fld['title'] + '\n')
-        os.mkdir("Box/" + fld['title'])
+        os.mkdir(arcgisboxdir + fld['title'])
         flds = user.items(folder=fld['title'])
         print('You have ' + str(len(flds)) + ' items inside your folder\n')
         for i in flds:
             if i.type in file_types:
                 n += 1
+                item_path = str(i.get_data())
+                os.rename(str(item_path), arcgisboxdir + fld['title'] + "/" + i.title)
                 print('\t\t' + i.title + '\t\t(' + i.type + ')\n')
 
     print('\tDownloadable elements:\t' + str(n))
-
-
-def CreateFolder():
-    print(os.getcwd())
 
 CreateFolderStructure()
