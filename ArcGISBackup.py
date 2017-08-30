a3aa065107bea0d351db9df694e2c79f70f1d9cf
Aitor Calero <aitor.calero@esri.es>
2017-08-30 22:01:15 +0200

    Code fixed to work well in Windows OS

--- ArcGISBackup.py
+++ ArcGISBackup.py
@@ -1,5 +1,3 @@
-import logging
-
 from arcgis.gis import GIS
 from arcgis.gis import User
 import os
@@ -12,14 +10,12 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S'
                     )
 
-
 # we open a text file where my credentials are stored
 # this file must be in the same directory
 with open('credentials.txt') as f:
     url, usr, p, boxdir = f.read().split('\n')
 
-arcgisboxdir = str(Path.home())+ boxdir
-logging.info(arcgisboxdir)
+
 
 # we connect to my arcgis account using the previous credentials
 def connect_to_arcgis(_url, _usr, _pwd):
@@ -27,16 +23,17 @@ def connect_to_arcgis(_url, _usr, _pwd):
     logging.info('Connected successfully to the {} organization\n'.format(gis.properties.name))
     return gis
 
-
 gis = connect_to_arcgis(url, usr, p)
-
 user = User(gis, usr, userdict=None)
+
+arcgisboxdir = os.path.normpath(str(Path.home())+ boxdir)
+logging.info(arcgisboxdir)
 
 # we make sure the the arcgisbox dir do not previously exists and delete it if so
 if Path(arcgisboxdir).exists():
     shutil.rmtree(arcgisboxdir)
 
-os.mkdir('Box')
+os.mkdir(arcgisboxdir)
 
 
 # we recursively loop through each folder downloading the items type given in file_types
@@ -58,22 +55,25 @@ def CreateFolderStructure(_user):
             n += 1
             item_path = str(root_folder_item.download())
             file_extension = item_path.rsplit(".",maxsplit=1)[1]
-            os.rename(str(item_path), arcgisboxdir + root_folder_item.title + "." + file_extension)
+            file_destination = os.path.normpath(arcgisboxdir + '/' + root_folder_item.title + "." + file_extension)
+            os.rename(str(item_path), file_destination)
             logging.info('{}\t\t({})\n'.format(root_folder_item.title,root_folder_item.type))
 
     # Listing documents inside other user folders
     for fld in flds:
         # logging.info((carpeta))
         logging.info('Name: {}\n'.format(fld['title']))
-        os.mkdir(arcgisboxdir + fld['title'])
+        os.mkdir(os.path.normpath( arcgisboxdir + "/" + fld['title']))
         flds = _user.items(folder=fld['title'])
         logging.info('You have {} items inside your folder\n'.format(str(len(flds))))
         for i in flds:
             if i.type in file_types:
                 n += 1
                 item_path = str(i.download())
-                file_extension = item_path.split(".")[1]
-                os.rename(str(item_path), arcgisboxdir + fld['title'] + "/" + i.title + "." + file_extension)
+                file_extension = item_path.rsplit(".",maxsplit=1)[1]
+                file_destination = os.path.normpath(arcgisboxdir +"/"+ fld['title'] + "/" + i.title + "." + file_extension)
+                logging.info(file_destination)
+                os.rename(str(item_path), file_destination)
                 logging.info('\t\t {} \t\t( {} )\n'.format(i.title,i.type))
 
     logging.info('\tDownloadable elements:\t {}'.format(str(n)))
